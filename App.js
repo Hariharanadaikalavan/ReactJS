f2d34a51bc7bbb92c189aefe2ae797d2a203364c
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-03 23:58:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+//import { League } from "./Task-3/League";
+//import { SocialButton } from "./Task-4/SocialButton";
+import { Notiication } from "./Task-5/Notiffication";
+// import { Congratscards } from "./Task-2/Congratscard";
+// import {Home} from './components/Home';
+// import {Hello} from './components/Home'
+//import 'bootstrap/dist/css/bootstrap.css'
+//import { faFontAwesome } from '@fortawesome/free-regular-svg-icons';
+//import {faUser,faEnvelope} from '@fortawesome/free-brands-svg-icons'
+
+ function App() {
+  return (
+    <>
+{/* <Home/>
+<Congratscards/>
+<League/> */}
+{/* <SocialButton/> */}
+
+<Notiication/>
+</>
+  );
+}
+
+export default App;
